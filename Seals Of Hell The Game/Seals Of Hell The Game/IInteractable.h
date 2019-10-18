5e67103e6b32000863a44905f48a42e4200202dd
dakshvir_rehill <dakshvirr@gmail.com>
2019-10-18 13:46:56 -0400

    finished pure abstract class interactable

--- Seals Of Hell The Game/Seals Of Hell The Game/IInteractable.h
+++ Seals Of Hell The Game/Seals Of Hell The Game/IInteractable.h
@@ -1,8 +1,22 @@
 #pragma once
 #ifndef I_INTERACTABLE_H
 #define I_INTERACTABLE_H
-class IInteractable
+#include "BasicObject.h"
+class IInteractable : public BasicObject
 {
-
+public:
+	virtual bool teleportRegion() = 0;
+	virtual bool pickObject() = 0;
+	virtual bool goDirection() = 0;
+	virtual bool blockAttack() = 0;
+	virtual bool attackEnemy() = 0;
+	virtual bool wearObject() = 0;
+	virtual bool playObject() = 0;
+	virtual bool giveObject() = 0;
+	virtual bool eatObject() = 0;
+	virtual bool moveObject() = 0;
+	virtual bool answerRiddle() = 0;
+	virtual bool dropObject() = 0;
+//	virtual bool giveString() = 0;
 };
 #endif
