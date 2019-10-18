6c4cc79774a10f5bfbe4501dcb5630e852cb3ad6
dakshvir_rehill <dakshvirr@gmail.com>
2019-10-18 16:04:17 -0400

    included all headers

--- Seals Of Hell The Game/Seals Of Hell The Game/GameManager.cpp
+++ Seals Of Hell The Game/Seals Of Hell The Game/GameManager.cpp
@@ -4,10 +4,13 @@
 #include "json.hpp"
 #include "Region.h"
 #include "Room.h"
+#include "Collector.h"
+#include "Enemy.h"
+#include "Gateway.h"
+#include "KillZone.h"
+#include "OneInteractionItem.h"
+#include "PickableItem.h"
 #include "Portal.h"
-
-
-
 
 void GameManager::StartGame(std::string& pFileName)
 {
