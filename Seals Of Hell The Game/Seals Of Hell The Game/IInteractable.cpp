0d87a30e28175b8be25ddac868b584b1731f3d52
dakshvir_rehill <dakshvirr@gmail.com>
2019-10-21 16:31:21 -0400

    changed give and attack object default code

--- Seals Of Hell The Game/Seals Of Hell The Game/IInteractable.cpp
+++ Seals Of Hell The Game/Seals Of Hell The Game/IInteractable.cpp
@@ -8,78 +8,65 @@ void IInteractable::lookObject()
 	}
 	std::cout << getName() << std::endl;
 	std::cout << getStory() << std::endl << std::endl;
-	
 }
 void IInteractable::teleportRegion()
 {
 	std::cout << getName() << " cannot be used to teleport." << std::endl;
-	
 }
 
 void IInteractable::pickObject()
 {
 	std::cout << getName() << " cannot be used picked up." << std::endl;
-	
 }
 
 void IInteractable::goDirection()
 {
 	std::cout << "Can't Find A Way There." << std::endl;
-	
 }
 
 void IInteractable::blockAttack()
 {
-	std::cout << getName() << " cannot be used to block attack." << std::endl;
-	
+	std::cout << getName() << " cannot be blocked." << std::endl;
 }
 
 void IInteractable::attackEnemy(IInteractable* pWeapon)
 {
-	std::cout << getName() << " cannot be used to attack enemy." << std::endl;
-	
+	std::cout << getName() << " cannot be attacked with "<< pWeapon->getName() << " ."<< std::endl;
 }
 
 void IInteractable::wearObject()
 {
 	std::cout << getName() << " cannot be worn." << std::endl;
-	
 }
 
 void IInteractable::playObject()
 {
 	std::cout << getName() << " cannot be played." << std::endl;
-	
 }
 
-void IInteractable::giveObject(IInteractable* pGiveable)
+void IInteractable::giveObject(IInteractable* pCollector)
 {
-	std::cout << getName() << " cannot be given." << std::endl;
-	
+	std::cout << getName() << " cannot be given to" << pCollector->getName() << " ."<< std::endl;
 }
 
 void IInteractable::eatObject()
 {
 	std::cout << getName() << " cannot be eaten." << std::endl;
-	
 }
 
 void IInteractable::moveObject()
 {
 	std::cout << getName() << " cannot be moved." << std::endl;
-	
 }
 
 void IInteractable::answerRiddle()
 {
 	std::cout << getName() << " cannot be answered." << std::endl;
-	
 }
 
 void IInteractable::dropObject()
 {
 	std::cout << getName() << " cannot be dropped." << std::endl;
-	
 }
 
 void IInteractable::giveLove()
