b418f94c3e3a07860a63ea43988648a308b0c6c5
dakshvir_rehill <dakshvirr@gmail.com>
2019-10-21 20:32:16 -0400

    added collector code

--- Seals Of Hell The Game/Seals Of Hell The Game/Collector.cpp
+++ Seals Of Hell The Game/Seals Of Hell The Game/Collector.cpp
@@ -1,1 +1,32 @@
 #include "Collector.h"
+#include<iostream>
+void Collector::update()
+{
+	if (isInteractable())
+	{
+		if (getConditionalObject()->isGiven())
+		{
+			enemyDeath();
+		}
+		else
+		{
+			std::cout << getName() << std::endl;
+			std::cout << getAttackStory() << std::endl;
+		}
+	}
+}
+
+void Collector::enemyDeath()
+{
+	bool aTemp = false;
+	makeInteractable(aTemp);
+	makeVisible(aTemp);
+	std::cout << getName() << std::endl;
+	std::cout << getDeathStory() << std::endl;
+	bool aVal = true;
+	for (auto& iter : getConditionUpdateObjects())
+	{
+		iter->makeInteractable(aVal);
+		iter->makeVisible(aVal);
+	}
+}
