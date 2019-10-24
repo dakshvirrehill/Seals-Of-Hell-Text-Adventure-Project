10d46653b1fcdc3f6c3dead32becb0e6a1c88af7
dakshvir_rehill <dakshvirr@gmail.com>
2019-10-24 12:33:00 -0400

    added corresponding classes

--- Seals Of Hell Data Editor/Seals Of Hell Data Editor/Room.cs
+++ Seals Of Hell Data Editor/Seals Of Hell Data Editor/Room.cs
@@ -11,6 +11,19 @@ namespace Seals_Of_Hell_Data_Editor
         public string mName { get; set; }
         public string mStory { get; set; }
         public string mRegion { get; set; }
+        public bool mHasCollectors { get; set; }
+        public bool mHasEnemies { get; set; }
+        public bool mHasKillzones { get; set; }
+        public bool mHasOneInteractionItems { get; set; }
+        public bool mHasPickableItems { get; set; }
+        public bool mHasPortals { get; set; }
+        public Dictionary<string, Gateway> mGateways;
+        public Dictionary<string, Collector> mCollector;
+        public Dictionary<string, Enemy> mEnemy;
+        public Dictionary<string, KillZone> mKillZone;
+        public Dictionary<string, OneInteractionItem> mOneInteractionItem;
+        public Dictionary<string, PickableItem> mPickableItem;
+        public Dictionary<string, Portal> mPortal;
         public Room()
         {
             
