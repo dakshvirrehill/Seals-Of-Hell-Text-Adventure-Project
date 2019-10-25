90a783374ee45c2df45c3fdf22980a9c787e9f45
dakshvir_rehill <dakshvirr@gmail.com>
2019-10-25 14:52:32 -0400

    removed previous editor design code

--- Seals Of Hell Data Editor/Seals Of Hell Data Editor/Room.cs
+++ Seals Of Hell Data Editor/Seals Of Hell Data Editor/Room.cs
@@ -10,20 +10,6 @@ namespace Seals_Of_Hell_Data_Editor
     {
         public string mName { get; set; }
         public string mStory { get; set; }
-        public bool mHasCollectors { get; set; }
-        public bool mHasEnemies { get; set; }
-        public bool mHasKillzones { get; set; }
-        public bool mHasOneInteractionItems { get; set; }
-        public bool mHasPickableItems { get; set; }
-        public bool mHasPortals { get; set; }
-        public TreasureCollector mTreasureCollector { get; set; }
-        public Dictionary<string, Gateway> mGateways { get; set; }
-        public Dictionary<string, Collector> mCollectors { get; set; }
-        public Dictionary<string, Enemy> mEnemies { get; set; }
-        public Dictionary<string, KillZone> mKillZones { get; set; }
-        public Dictionary<string, OneInteractionItem> mOneInteractionItems { get; set; }
-        public Dictionary<string, PickableItem> mPickableItems { get; set; }
-        public Dictionary<string, Portal> mPortals { get; set; }
         public Room()
         {
             mName = "Default Name";
