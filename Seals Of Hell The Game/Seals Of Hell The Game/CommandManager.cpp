38852804374aceb78fa58221523a8f6236dff061
dakshvir_rehill <dakshvirr@gmail.com>
2019-11-01 18:11:15 -0400

    added db to visual tool and json to editor also added strip spaces from front and back in cm

--- Seals Of Hell The Game/Seals Of Hell The Game/CommandManager.cpp
+++ Seals Of Hell The Game/Seals Of Hell The Game/CommandManager.cpp
@@ -45,22 +45,20 @@ void CommandManager::help()
 	std::cout << "SAVE" << std::endl;
 	std::cout << "EXIT" << std::endl;
 	std::cout << "INVENTORY" << std::endl;
-	//std::cout << "WAKE UP" << std::endl;
 	std::cout << "LOOK" << std::endl;
-	std::cout << "LOOK <Object>" << std::endl;
-	std::cout << "TELEPORT <Region>" << std::endl;
-	std::cout << "PICK <Object>" << std::endl;
+	std::cout << "LOOK <Object Name>" << std::endl;
+	std::cout << "TELEPORT <Portal Name>" << std::endl;
+	std::cout << "PICK <Object Name>" << std::endl;
 	std::cout << "GO <Direction> (Example: GO North)" << std::endl;
-	std::cout << "ATTACK <Enemy> WITH <Weapon>" << std::endl;
-	std::cout << "BLOCK <Enemy>" << std::endl;
-	std::cout << "WEAR <Object>" << std::endl;
-	std::cout << "PLAY <Object>" << std::endl;
-	std::cout << "GIVE <Object> TO <Collector>" << std::endl;
-	std::cout << "EAT <Object>" << std::endl;
-	std::cout << "MOVE <Object>" << std::endl;
-	std::cout << "ANSWER <Answer>" << std::endl;
-	std::cout << "DROP <Object>" << std::endl;
-	//std::cout << "GIVE <Enemy> Love/Hate" << std::endl;
+	std::cout << "ATTACK <Enemy Name> WITH <Weapon Name>" << std::endl;
+	std::cout << "BLOCK <Enemy Name>" << std::endl;
+	std::cout << "WEAR <Object Name>" << std::endl;
+	std::cout << "PLAY <Object Name>" << std::endl;
+	std::cout << "GIVE <Object Name> TO <Collector Name>" << std::endl;
+	std::cout << "EAT <Object Name>" << std::endl;
+	std::cout << "MOVE <Object Name>" << std::endl;
+	std::cout << "ANSWER <Answer Name>" << std::endl;
+	std::cout << "DROP <Object Name>" << std::endl;
 	std::cout << "==============================" << std::endl;
 }
 
@@ -78,6 +76,20 @@ std::vector<std::string> CommandManager::getCommandWords(std::string& pCommandSt
 
 void CommandManager::convertToUpper(std::string& pString)
 {
+	if (pString.size() > 0)
+	{
+		if (pString[0] == ' ')
+		{
+			pString = pString.substr(1);
+		}
+	}
+	if (pString.size() > 0)
+	{
+		if (pString[pString.size() - 1] == ' ')
+		{
+			pString = pString.substr(0, pString.size() - 1);
+		}
+	}
 	for (size_t aI = 0; aI < pString.size(); aI++)
 	{
 		if (std::islower(pString[aI]))
@@ -110,7 +122,6 @@ bool CommandManager::runCommand(std::string& pCommandStr)
 	std::string aObj2 = "";
 	bool aSInObj2 = false;
 	bool aSwapObj1n2 = false;
-	//bool aLHF = false;
 	for (size_t aI = 1; aI < aCommandWords.size(); aI++)
 	{
 		if (aCommandWords.at(aI) == "WITH")
